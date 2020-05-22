fa3ce3e6d6ad9b58acc87ee929ff8409485cb6ae
Mark Harrison <mark.harrison@octopus.com>
2020-05-22 15:38:38 +0100

    Add gcp linux worker register script

---
+++ gcp/bootstrap/gcp-linux-worker-install-and-register.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+configFilePath="/etc/octopus/default/tentacle-default.config"
+
+if [[ -f $configFilePath ]]; then
+  echo "Tentacle already installed"
+else
+  octopusServerUrl=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/octopusServerUrl -H "Metadata-Flavor: Google")
+  octopusSpace=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/octopusSpace -H "Metadata-Flavor: Google")
+  octopusApiKey=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/octopusApiKey -H "Metadata-Flavor: Google")
+  workerPoolName=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/workerPoolName -H "Metadata-Flavor: Google")
+  octopusMachineName=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/octopusMachineName -H "Metadata-Flavor: Google")
+  serverCommsPort=10943            
+  applicationPath="/home/Octopus/Applications/"
+    
+  apt-key adv --fetch-keys https://apt.octopus.com/public.key
+  add-apt-repository "deb https://apt.octopus.com/ stretch main"
+  apt-get update
+   
+  apt-get install tentacle
+
+  /opt/octopus/tentacle/Tentacle create-instance --config "$configFilePath"
+  /opt/octopus/tentacle/Tentacle new-certificate --if-blank
+  /opt/octopus/tentacle/Tentacle configure --noListen True --reset-trust --app "$applicationPath"
+  echo "Registering the Tentacle $octopusMachineName with server $octopusServerUrl in worker pool $workerPoolName"
+  /opt/octopus/tentacle/Tentacle register-worker --server "$octopusServerUrl" --space "$octopusSpace" --apiKey "$octopusApiKey" --name "$octopusMachineName" --workerPool "$workerPoolName" --comms-style "TentacleActive" --server-comms-port $serverCommsPort
+  /opt/octopus/tentacle/Tentacle service --install --start
+  
+  sudo apt-get install -y curl apt-transport-https
+  
+  echo "Installing Powershell core"
+  snap install powershell --classic
+  
+fi
