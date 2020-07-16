d447cc742931ed7d7cfb3716c2cb67c91e8142a6
Lianne Crocker <56539139+octocrock@users.noreply.github.com>
2020-07-16 15:27:08 +0100

    Update gcp-linux-listening-worker-install-and-register-docker.sh

--- gcp/bootstrap/gcp-linux-listening-worker-install-and-register-docker.sh
+++ gcp/bootstrap/gcp-linux-listening-worker-install-and-register-docker.sh
@@ -44,11 +44,11 @@ else
   
   # Install Docker
   apt-get update
-  apt-get install apt-transport-https ca-certificates curl software-properties-common
+  apt-get -y install apt-transport-https ca-certificates curl software-properties-common
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
   apt-get update
-  apt-get install docker-ce docker-ce-cli containerd.io
+  apt-get -y install docker-ce docker-ce-cli containerd.io
   
   if [[ ! -z "$additionalCommands" ]]; then 
     echo "Running additional commands: $additionalCommands"
