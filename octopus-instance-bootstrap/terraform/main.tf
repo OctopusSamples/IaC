4559802811dfe7a36e8a8760ac06093fcf614fd8
BobJWalker <bob.walker@octopus.com>
2021-12-10 10:33:19 -0600

    Adding TF files for research

---
+++ octopus-instance-bootstrap/terraform/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    octopusdeploy = {
+      source = "OctopusDeployLabs/octopusdeploy"
+      version = "0.7.63" # example: 0.7.62
+    }
+  }
+}
+
+provider "octopusdeploy" {
+  # configuration options
+  address    = var.octopus_address
+  api_key    = var.octopus_api_key
+  space_id   = var.octopus_space_id
+  space_name = var.octopus_space_name
+}
+
+resource "octopusdeploy_environment" "test" {
+    name = "Test"
+}
+
+resource "octopusdeploy_environment" "test" {
+    name = "Production"
+}
+
+resource "octopusdeploy_lifecycle" "default_lifecycle" {
+    name = "Default"
+
+    release_retention_policy {
+        quantity_to_keep = 5
+        unit = "Days"        
+    }
+
+    tentacle_retention_policy {
+        quantity_to_keep = 5
+        unit = "Days"
+    }
+}
---
+++ octopus-instance-bootstrap/terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "octopus_address" {
+    type = string
+}
+
+variable "octopus_api_key" {
+    type = string
+}
+
+variable "octopus_space_id" {
+    type = string
+}
+
+variable "octopus_space_name" {
+    type = string
+}
