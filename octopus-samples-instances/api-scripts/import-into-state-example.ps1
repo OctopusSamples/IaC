6fcff29cb8f61bbd8b1f98064576aa89d0dc4719
BobJWalker <bob.walker@octopus.com>
2022-01-11 15:01:49 -0600

    Updating the import into state example to be more modular

--- octopus-samples-instances/api-scripts/import-into-state-example.ps1
+++ octopus-samples-instances/api-scripts/import-into-state-example.ps1
@@ -67,13 +67,69 @@ function Invoke-OctopusApi
     }    
 }
 
+function Get-ItemExistsInState
+{
+	param 
+    (
+    	$currentState,
+        $itemTypeToFind,
+        $itemNameToFind
+    )
+    
+    foreach ($item in $currentState.Values.root_module.resources)
+    {
+        Write-Host "Comparing $($item.Name) with $itemNameToFind and $($item.type) with $itemTypeToFind"
+        if ($item.name.ToLower().Trim() -eq $itemNameToFind.ToLower().Trim() -and $item.type.ToLower().Trim() -eq $itemTypeToFind)
+        {
+            Write-Host "The item already exists in the state"
+			return $true
+        }
+    }   
+    
+    return $false
+}
+
+function Get-OctopusFeedId
+{
+	param (
+    	$externalFeedList,
+    	$feedType,
+        $feedUrl
+    )
+    
+	foreach ($feed in $externalFeedList)
+    {
+        Write-Host "Verifying the $($feed.FeedType) matches $feedType, $($feed.FeedUri) matches $feedUrl and that the $($feed.Username) is null or empty"
+        if ($feed.FeedType -eq $feedType -and $feed.FeedUri -eq $feedUrl -and [string]::IsNullOrWhiteSpace($feed.Username))
+        {
+            Write-Host "The feed in question currently exists already.  Going to import it into state."
+            return $($feed.Id)            
+        }
+    }
+}
+
+function Add-ItemToState
+{
+	param (
+    	$itemTerraformAddress,
+        $itemOctopusId,
+        $itemName
+    )
+    
+    if ([string]::IsNullOrWhiteSpace($itemOctopusId))
+    {
+    	Write-Host "The item $itemName does not already exist in Octopus.  Not going to import it into state.  This will be created the next time apply runs."
+        return
+    }
+    
+    Write-Host "Importing $itemName into the terraform state with the address $itemTerraformAddress and Id $itemOctopusId"
+    terraform import $itemTerraformAddress $itemOctopusId     
+}
+
 # The Octopus Instance information to modify
 $octopusUrl = $OctopusParameters["Samples.Octopus.Url"]
 $octopusApiKey = $OctopusParameters["Samples.Octopus.Api.Key"]
 $spaceId = $OctopusParameters["Project.Standards.SpaceId"]
-
-$feedType = "Docker"
-$feedUrl = "https://index.docker.io"
 
 # Get the working directory for Terraform
 $extractedDirectory = $OctopusParameters["Octopus.Action.Package[IaC].ExtractedPath"]
@@ -116,52 +172,25 @@ Write-Host "Pulling in the current state"
 $currentStateAsJson = terraform show -json
 $currentState = $currentStateAsJson | ConvertFrom-Json -depth 10
 
-$foundItem = $false
-$stateNameToFind = "docker"
+$dockerFeedNameToFind = "docker"
+$gitHubFeednameToFind = "github"
 $stateTypeToFind = "octopusdeploy_feed"
-$terraformId = "$stateTypeToFind.$stateNameToFind"
 
-foreach ($item in $currentState.Values.root_module.resources)
-{
-	Write-Host "Comparing $($item.Name) with $stateNameToFind and $($item.type) with $stateTypeToFind"
-	if ($item.name.ToLower().Trim() -eq $stateNameToFind.ToLower().Trim() -and $item.type.ToLower().Trim() -eq $stateTypeToFind)
-    {
-    	Write-Host "The item already exists in the state"
-        $foundItem = $true
-        break
-    }
-}
+$foundDockerFeed = Get-ItemExistsInState -currentState $currentState -itemTypeToFind $stateTypeToFind -itemNameToFind $dockerFeedNameToFind
+$foundGitHubFeed = Get-ItemExistsInState -currentState $currentState -itemTypeToFind $stateTypeToFind -itemNameToFind $gitHubFeednameToFind
 
-if ($foundItem -eq $true)
+if ($foundDockerFeed -eq $true -and $foundGitHubFeed -eq $true)
 {
-	Write-Host "The item already exists in the state file, no need to continue.  Exiting."
+	Write-Host "Both feeds already exists in the state file, no need to continue.  Exiting."
     exit 0
 }
 
-Write-Host "The external feed was not found in the state file.  Checking to see if that feed exists currently."
+Write-Host "One or more of the external feeds was not found in the state file.  Checking to see if they already exist and if they do, import them into state."
 $rawExternalFeedsList = Invoke-OctopusApi -OctopusUrl $octopusUrl -endPoint "feeds?feedType=Nuget,Docker,Maven,GitHub,Helm,AwsElasticContainerRegistry&skip=0&take=1000" -spaceId $spaceId -apiKey $OctopusApiKey -item $null -method "GET"
 $externalFeedList = $rawExternalFeedsList.Items
 
-$foundFeed = $false
-foreach ($feed in $externalFeedList)
-{
-	Write-Host "Verifying the $($feed.FeedType) matches $feedType, $($feed.FeedUri) matches $feedUrl and that the $($feed.Username) is null or empty"
-	if ($feed.FeedType -eq $feedType -and $feed.FeedUri -eq $feedUrl -and [string]::IsNullOrWhiteSpace($feed.Username))
-    {
-    	Write-Host "The feed in question currently exists already.  Going to import it into state."
-        Write-host "Terraform import ""$terraformId"" $($feed.Id)"
-        terraform import $terraformId $($feed.Id)
-        $foundFeed = $true
-        break
-    }
-}
+$existingDockerFeedId = Get-OctopusFeedId -externalFeedList $externalFeedList -feedType "Docker" -feedUrl "https://index.docker.io"
+Add-ItemToState -itemTerraformAddress "$stateTypeToFind.$dockerFeedNameToFind" -itemOctopusId $existingDockerFeedId -itemName "Docker External Feed"
 
-if ($foundFeed)
-{
-	Write-Host "Finished importing the existing feed into Terraform"
-}
-else
-{
-	Write-Host "The feed you want to import does not exist, the next time the apply command runs it will create a new feed for you."
-}
-
+$existingGitHubFeedId = Get-OctopusFeedId -externalFeedList $externalFeedList -feedType "GitHub" -feedUrl "https://api.github.com"
+Add-ItemToState -itemTerraformAddress "$stateTypeToFind.$gitHubFeednameToFind" -itemOctopusId $existingGitHubFeedId -itemName "GitHub External Feed"
