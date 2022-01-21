97f4e26d1c84baa2661af522ee4595e67867d522
Bob Walker <Bob.Walker@octopus.com>
2022-01-21 14:46:58 -0600

    More GCP Variables

--- octopus-samples-instances/terraform/gcp-library-variable-set.tf
+++ octopus-samples-instances/terraform/gcp-library-variable-set.tf
@@ -11,3 +11,102 @@ resource "octopusdeploy_variable" "gcp_variable_set_worker_pool" {
   value = octopusdeploy_static_worker_pool.gcp_worker_pool.id
   owner_id = octopusdeploy_library_variable_set.gcp_variable_set.id
 }
+
+resource "octopusdeploy_variable" "gcp_variable_project_name" {
+  name = "GCP.Project.Name"
+  type = "String"
+  is_editable = true
+  is_sensitive = false
+  value = "octopus-samples"
+  owner_id = octopusdeploy_library_variable_set.gcp_variable_set.id
+}
+
+resource "octopusdeploy_variable" "gcp_variable_us_region_name" {
+  name = "GCP.US.Region.Name"
+  type = "String"
+  is_editable = true
+  is_sensitive = false
+  value = "us-central1"
+  owner_id = octopusdeploy_library_variable_set.gcp_variable_set.id
+}
+
+resource "octopusdeploy_variable" "gcp_variable_us_zone_primary_name" {
+  name = "GCP.US.Zone.Primary.Name"
+  type = "String"
+  is_editable = true
+  is_sensitive = false
+  value = "us-central1-c"
+  owner_id = octopusdeploy_library_variable_set.gcp_variable_set.id
+}
+
+resource "octopusdeploy_variable" "gcp_variable_us_zone_secondary_name" {
+  name = "GCP.US.Zone.Secondary.Name"
+  type = "String"
+  is_editable = true
+  is_sensitive = false
+  value = "us-central1-b"
+  owner_id = octopusdeploy_library_variable_set.gcp_variable_set.id
+}
+
+resource "octopusdeploy_variable" "gcp_variable_uk_region_name" {
+  name = "GCP.UK.Region.Name"
+  type = "String"
+  is_editable = true
+  is_sensitive = false
+  value = "europe-west2"
+  owner_id = octopusdeploy_library_variable_set.gcp_variable_set.id
+}
+
+resource "octopusdeploy_variable" "gcp_variable_uk_zone_primary_name" {
+  name = "GCP.UK.Zone.Primary.Name"
+  type = "String"
+  is_editable = true
+  is_sensitive = false
+  value = "europe-west2-b"
+  owner_id = octopusdeploy_library_variable_set.gcp_variable_set.id
+}
+
+resource "octopusdeploy_variable" "gcp_variable_uk_zone_secondary_name" {
+  name = "GCP.UK.Zone.Secondary.Name"
+  type = "String"
+  is_editable = true
+  is_sensitive = false
+  value = "europe-west2-a"
+  owner_id = octopusdeploy_library_variable_set.gcp_variable_set.id
+}
+
+resource "octopusdeploy_variable" "gcp_variable_linux_os_image" {
+  name = "GCP.Instance.Linux.Image"
+  type = "String"
+  is_editable = true
+  is_sensitive = false
+  value = "ubuntu-os-cloud/ubuntu-2004-lts"
+  owner_id = octopusdeploy_library_variable_set.gcp_variable_set.id
+}
+
+resource "octopusdeploy_variable" "gcp_variable_linux_default_vm_size" {
+  name = "GCP.Instance.Linux.VM.Size"
+  type = "String"
+  is_editable = true
+  is_sensitive = false
+  value = "g1-small"
+  owner_id = octopusdeploy_library_variable_set.gcp_variable_set.id
+}
+
+resource "octopusdeploy_variable" "gcp_variable_windows_os_image" {
+  name = "GCP.Instance.Linux.Image"
+  type = "String"
+  is_editable = true
+  is_sensitive = false
+  value = "windows-cloud/windows-2019"
+  owner_id = octopusdeploy_library_variable_set.gcp_variable_set.id
+}
+
+resource "octopusdeploy_variable" "gcp_variable_windows_default_vm_size" {
+  name = "GCP.Instance.Windows.VM.Size"
+  type = "String"
+  is_editable = true
+  is_sensitive = false
+  value = "n1-standard-1"
+  owner_id = octopusdeploy_library_variable_set.gcp_variable_set.id
+}
