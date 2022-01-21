9026acc77a02601fa5ff6c21a8db7108e5a66f30
Bob Walker <Bob.Walker@octopus.com>
2022-01-21 09:43:53 -0600

    Updating Provider Version Number, Adding GCP Variable Set

---
+++ octopus-samples-instances/terraform/gcp-library-variable-set.tf
@@ -0,0 +1,13 @@
+resource "octopusdeploy_library_variable_set" "gcp_variable_set" {
+  name = "GCP TF"
+  description = "Library variable set storing GCP specific items you can leverage in your deployment process."
+}
+
+resource "octopusdeploy_variable" "azure_worker_pool_id_variable" {
+  name = "GCP.WorkerPool"
+  type = "WorkerPool"
+  is_editable = true
+  is_sensitive = false
+  value = octopusdeploy_static_worker_pool.gcp_worker_pool.id
+  owner_id = octopusdeploy_library_variable_set.gcp_variable_set.id
+}
