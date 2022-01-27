348a43a2d495236a83f41d95dca02d90641a1d25
Bob Walker <Bob.Walker@octopus.com>
2022-01-27 16:01:12 -0600

    Locking down the SQL Server

--- azure/terraform/azure-sql.tf
+++ azure/terraform/azure-sql.tf
@@ -1,9 +1,9 @@
 resource "azurerm_storage_account" "permanent" {
-    name                        = format("sql%s", replace(var.azure_sql_name, "-", ""))
-    resource_group_name         = azurerm_resource_group.permanent.name
-    location                    = azurerm_resource_group.permanent.location
-    account_tier                = "Standard"
-    account_replication_type    = "LRS"
+    name                            = format("sql%s", replace(var.azure_sql_name, "-", ""))
+    resource_group_name             = azurerm_resource_group.permanent.name
+    location                        = azurerm_resource_group.permanent.location
+    account_tier                    = "Standard"
+    account_replication_type        = "LRS"
 
     tags = {
         LifetimeInDays = 365
@@ -11,14 +11,22 @@ resource "azurerm_storage_account" "permanent" {
 }
 
 resource "azurerm_mssql_server" "permanent" {
-    name                         = var.azure_sql_name
-    resource_group_name          = azurerm_resource_group.permanent.name
-    location                     = azurerm_resource_group.permanent.location
-    version                      = "12.0"
-    administrator_login          = var.azure_sql_administrator_name
-    administrator_login_password = var.azure_sql_administrator_password
+    name                            = var.azure_sql_name
+    resource_group_name             = azurerm_resource_group.permanent.name
+    location                        = azurerm_resource_group.permanent.location
+    version                         = "12.0"
+    administrator_login             = var.azure_sql_administrator_name
+    administrator_login_password    = var.azure_sql_administrator_password
+    minimum_tls_version             = "1.2"
+    public_network_access_enabled   = false
 
     tags = {
         LifetimeInDays = 365
     }
+}
+
+resource "azurerm_mssql_virtual_network_rule" "permanent" {
+    name                            = format("%s-vnet-rule", var.azure_sql_name, "-", "")
+    server_id                       = azurerm_mssql_server.permanent.id
+    subnet_id                       = azurerm_subnet.default.id
 }
