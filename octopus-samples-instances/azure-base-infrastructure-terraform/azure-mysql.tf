8663b52b2e12054b601ed480fdf0dd048d90d033
twerthi <twerthi@hotmail.com>
2022-02-07 14:44:32 -0800

    Fixing resource errors

--- octopus-samples-instances/azure-base-infrastructure-terraform/azure-mysql.tf
+++ octopus-samples-instances/azure-base-infrastructure-terraform/azure-mysql.tf
@@ -9,10 +9,10 @@ resource "azurerm_mysql_server" "permanent" {
     sku_name                         = "B_Gen5_1"
     version                          = "5.7"
     storage_mb                       = 5120
-    backup_retention_days            = 0
+    backup_retention_days            = 7
     geo_redundant_backup_enabled     = false
     auto_grow_enabled                = false
     public_network_access_enabled    = false
     ssl_enforcement_enabled          = true
-    ssl_minimal_tls_version_enforced = "TLS1.2"
+    ssl_minimal_tls_version_enforced = "TLS1_2"
 }
