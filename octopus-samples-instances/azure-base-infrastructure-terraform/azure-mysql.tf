6e479ef7e80b4ee1e033ba4f7f625f0630ee94c0
twerthi <twerthi@hotmail.com>
2022-02-08 14:48:22 -0800

    Updating MySQL version

--- octopus-samples-instances/azure-base-infrastructure-terraform/azure-mysql.tf
+++ octopus-samples-instances/azure-base-infrastructure-terraform/azure-mysql.tf
@@ -7,7 +7,7 @@ resource "azurerm_mysql_server" "permanent" {
     administrator_login_password     = var.azure_mysql_administrator_password
 
     sku_name                         = "B_Gen5_1"
-    version                          = "5.7"
+    version                          = "8.0"
     storage_mb                       = 5120
     backup_retention_days            = 7
     geo_redundant_backup_enabled     = false
