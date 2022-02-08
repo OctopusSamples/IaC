3dedfef0b3a994484d1824b8b9dd05e4addd53da
twerthi <twerthi@hotmail.com>
2022-02-08 12:48:03 -0800

    Adjusting resource name to satisfy character limits

--- octopus-samples-instances/azure-base-infrastructure-terraform/azure-sql.tf
+++ octopus-samples-instances/azure-base-infrastructure-terraform/azure-sql.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "permanent" {
-    name                            = format("sql%s", replace(var.azure_sql_name, "-", ""))
+    name                            = format("%s_sa", replace(azurerm_resource_group.permanent.name, "_", ""))
     resource_group_name             = azurerm_resource_group.permanent.name
     location                        = azurerm_resource_group.permanent.location
     account_tier                    = "Standard"
