e74177826fd0e4eacecb596e3ee6cc8b8ef493cb
twerthi <twerthi@hotmail.com>
2022-08-15 15:07:34 -0700

    Octopus Deploy Terraform provider updated resource 'octopusdeploy_feed' to 'octopusdeploy_feeds', updating Terraform to comply

--- octopus-samples-instances/octopus-space-standards-terraform/external-feeds.tf
+++ octopus-samples-instances/octopus-space-standards-terraform/external-feeds.tf
@@ -1,17 +1,17 @@
-resource "octopusdeploy_feed" "github" {
+resource "octopusdeploy_feeds" "github" {
   name = "GitHub Feed TF"  
   feed_type = "GitHub"
   feed_uri = "https://api.github.com"
   is_enhanced_mode = false
 }
 
-resource "octopusdeploy_feed" "feedz" {
+resource "octopusdeploy_feeds" "feedz" {
   name = "Feedz Feed TF"  
   feed_type = "NuGet"
   feed_uri = "https://f.feedz.io/octopus-deploy-samples/octopus-samples/nuget/index.json"
 }
 
-resource "octopusdeploy_feed" "docker" {
+resource "octopusdeploy_feeds" "docker" {
   name = "Docker Feed TF"
   feed_type = "Docker"
   feed_uri = "https://index.docker.io"
