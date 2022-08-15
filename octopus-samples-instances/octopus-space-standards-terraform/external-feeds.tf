02d646edecfa21047caaf307e31bfa4d342526f0
twerthi <twerthi@hotmail.com>
2022-08-15 15:18:26 -0700

    Upating feed declarations to be specific

--- octopus-samples-instances/octopus-space-standards-terraform/external-feeds.tf
+++ octopus-samples-instances/octopus-space-standards-terraform/external-feeds.tf
@@ -1,21 +1,21 @@
-resource "octopusdeploy_feeds" "github" {
+resource "octopusdeploy_github_repository_feed" "github" {
   name = "GitHub Feed TF"  
-  feed_type = "GitHub"
+  #feed_type = "GitHub"
   feed_uri = "https://api.github.com"
   is_enhanced_mode = false
 }
 
-resource "octopusdeploy_feeds" "feedz" {
+resource "octopusdeploy_nuget_feed" "feedz" {
   name = "Feedz Feed TF"  
-  feed_type = "NuGet"
+  #feed_type = "NuGet"
   feed_uri = "https://f.feedz.io/octopus-deploy-samples/octopus-samples/nuget/index.json"
 }
 
-resource "octopusdeploy_feeds" "docker" {
+resource "octopusdeploy_docker_container_registry" "docker" {
   name = "Docker Feed TF"
-  feed_type = "Docker"
+  #feed_type = "Docker"
   feed_uri = "https://index.docker.io"
-  is_enhanced_mode = false
-  download_attempts = 0
-  download_retry_backoff_seconds = 0
+  #is_enhanced_mode = false
+  #download_attempts = 0
+  #download_retry_backoff_seconds = 0
 }
