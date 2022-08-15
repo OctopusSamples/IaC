77997c7025bc8aa66fcc96e191fab92615b97059
twerthi <twerthi@hotmail.com>
2022-08-15 14:08:06 -0700

    Associating vpc security groups to rds instances

--- octopus-samples-instances/aws-base-infrastructure-terraform/rds.tf
+++ octopus-samples-instances/aws-base-infrastructure-terraform/rds.tf
@@ -15,6 +15,7 @@ resource "aws_db_instance" "mariadb_rds_instance" {
   iam_database_authentication_enabled = true
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.samples_rds_subnet_group.name
+  vpc_security_group_ids = "[${aws_security_group.samples_security_group.id}]"
 }
 
 resource "aws_db_instance" "mysql_rds_instance" {
@@ -28,6 +29,7 @@ resource "aws_db_instance" "mysql_rds_instance" {
   iam_database_authentication_enabled = true
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.samples_rds_subnet_group.name
+  vpc_security_group_ids = "[${aws_security_group.samples_security_group.id}]"
 }
 
 resource "aws_db_instance" "postgresql_rds_instance" {
@@ -41,6 +43,7 @@ resource "aws_db_instance" "postgresql_rds_instance" {
   iam_database_authentication_enabled = true
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.samples_rds_subnet_group.name
+  vpc_security_group_ids = "[${aws_security_group.samples_security_group.id}]"
 }
 
 resource "aws_db_instance" "mssql_rds_instance" {
@@ -54,4 +57,5 @@ resource "aws_db_instance" "mssql_rds_instance" {
   skip_final_snapshot  = true
   license_model        = "license-included"
   db_subnet_group_name = aws_db_subnet_group.samples_rds_subnet_group.name
+  vpc_security_group_ids = "[${aws_security_group.samples_security_group.id}]"
 }
