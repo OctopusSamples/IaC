55bdd1fc5abf54d23d3587ca386deb26e3ac8399
twerthi <twerthi@hotmail.com>
2022-09-08 09:41:38 -0700

    Adding additional software to powershell configure-tentacle script.

--- octopus-samples-instances/shared-workers-terraform/configure-tentacle.ps1
+++ octopus-samples-instances/shared-workers-terraform/configure-tentacle.ps1
@@ -41,6 +41,12 @@ Write-Host "Finished register worker..."
 # Install .net 6
 choco install dotnet-6.0-sdk -y
 
+# Install PowerShell Core
+choco install powershell-core -y
+
+# Install JRE
+choco install javaruntime -y
+
 # Install docker engine
 #Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1" -o install-docker-ce.ps1
 #.\install-docker-ce.ps1
