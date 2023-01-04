08d5453890af5f7e3df39c0f6cabc30549ac74ee
Cory Reid <cory.reid@octopus.com>
2023-01-04 15:27:07 -0500

    Migration: postgresql_server to postgresql_flexible_server

--- octopus-samples-instances/azure-base-infrastructure-terraform/azure-postgresql.tf
+++ octopus-samples-instances/azure-base-infrastructure-terraform/azure-postgresql.tf
@@ -1,30 +1,27 @@
-resource "azurerm_postgresql_server" "permanent" {
-    name                             = var.azure_postresql_name
-    resource_group_name              = azurerm_resource_group.permanent.name
-    location                         = azurerm_resource_group.permanent.location
-    
-    administrator_login              = var.azure_postgresql_administrator_name
-    administrator_login_password     = var.azure_postgresql_administrator_password
+resource "azurerm_postgresql_flexible_server" "permanent" {
+  name                = var.azure_postresql_name
+  resource_group_name = azurerm_resource_group.permanent.name
+  location            = azurerm_resource_group.permanent.location
 
-    sku_name                         = "B_Gen5_1"
-    version                          = "11"
-    storage_mb                       = 5120
-    backup_retention_days            = 7
-    geo_redundant_backup_enabled     = false
-    auto_grow_enabled                = false
-    public_network_access_enabled    = true
-    ssl_enforcement_enabled          = true
-    ssl_minimal_tls_version_enforced = "TLS1_2"
+  administrator_login    = var.azure_postgresql_administrator_name
+  administrator_password = var.azure_postgresql_administrator_password
 
-    tags = {
-        LifetimeInDays = 365
-    }    
+  sku_name                     = "B_Standard_B1ms"
+  version                      = "11"
+  storage_mb                   = 5120
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = false
+
+  tags = {
+    LifetimeInDays = 365
+  }
+
 }
 
-resource "azurerm_postgresql_firewall_rule" "all_azure_resources" {
-  name                = "all-azure-resources"
-  resource_group_name = azurerm_resource_group.permanent.name
-  server_name         = azurerm_postgresql_server.permanent.name
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0"
+resource "azurerm_postgresql_flexible_server_firewall_rule" "all_azure_resources" {
+  name      = "all-azure-resources"
+  server_id = azurerm_postgresql_flexible_server.permanent.id
+
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
 }
