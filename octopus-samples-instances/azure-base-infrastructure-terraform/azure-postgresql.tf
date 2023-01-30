f48a63a29f480bdcb6ab30a2ee3660df53ae8179
Mark Harrison <mark.harrison@octopus.com>
2023-01-30 09:57:54 +0000

    Update postgresql_flexible_server max storage

--- octopus-samples-instances/azure-base-infrastructure-terraform/azure-postgresql.tf
+++ octopus-samples-instances/azure-base-infrastructure-terraform/azure-postgresql.tf
@@ -8,7 +8,7 @@ resource "azurerm_postgresql_flexible_server" "permanent" {
 
   sku_name                     = "B_Standard_B1ms"
   version                      = "11"
-  storage_mb                   = 5120
+  storage_mb                   = 32768
   backup_retention_days        = 7
   geo_redundant_backup_enabled = false
 
