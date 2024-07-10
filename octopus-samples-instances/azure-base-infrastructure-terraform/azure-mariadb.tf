3eccd4c966ff57d6c49d51b44ca4a341ef69b081
twerthi <twerthi@hotmail.com>
2024-07-10 16:06:52 -0700

    MariaDB is deprecated on Azure, boo

--- octopus-samples-instances/azure-base-infrastructure-terraform/azure-mariadb.tf
+++ octopus-samples-instances/azure-base-infrastructure-terraform/azure-mariadb.tf
@@ -1,27 +1,27 @@
-resource "azurerm_mariadb_server" "permanent" {
-  name                = var.azure_mariadb_name
-  resource_group_name = azurerm_resource_group.permanent.name
-  location            = azurerm_resource_group.permanent.location
+# resource "azurerm_mariadb_server" "permanent" {
+#   name                = var.azure_mariadb_name
+#   resource_group_name = azurerm_resource_group.permanent.name
+#   location            = azurerm_resource_group.permanent.location
 
-  administrator_login    = var.azure_mariadb_administrator_name
-  administrator_login_password = var.azure_mariadb_administrator_password
+#   administrator_login    = var.azure_mariadb_administrator_name
+#   administrator_login_password = var.azure_mariadb_administrator_password
 
-  sku_name                     = "B_Gen5_1"
-  version                      = "10.3"
-  backup_retention_days        = 7
-  geo_redundant_backup_enabled = false
-  ssl_enforcement_enabled = true
-  storage_mb = 5120
+#   sku_name                     = "B_Gen5_1"
+#   version                      = "10.3"
+#   backup_retention_days        = 7
+#   geo_redundant_backup_enabled = false
+#   ssl_enforcement_enabled = true
+#   storage_mb = 5120
 
-  tags = {
-    LifetimeInDays = 365
-  }
-}
+#   tags = {
+#     LifetimeInDays = 365
+#   }
+# }
 
-resource "azurerm_mariadb_firewall_rule" "all_azure_resources" {
-  name                = "all-azure-resources"
-  resource_group_name = azurerm_resource_group.permanent.name
-  server_name         = azurerm_mysql_flexible_server.permanent.name
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0"
-}
+# resource "azurerm_mariadb_firewall_rule" "all_azure_resources" {
+#   name                = "all-azure-resources"
+#   resource_group_name = azurerm_resource_group.permanent.name
+#   server_name         = azurerm_mysql_flexible_server.permanent.name
+#   start_ip_address    = "0.0.0.0"
+#   end_ip_address      = "0.0.0.0"
+# }
