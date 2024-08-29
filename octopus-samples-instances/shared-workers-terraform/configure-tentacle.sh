339dff59f1a555555d9000198cce35925c1ff1d2
twerthi <twerthi@hotmail.com>
2024-08-29 16:21:45 -0700

    Installing helm to linux workers

--- octopus-samples-instances/shared-workers-terraform/configure-tentacle.sh
+++ octopus-samples-instances/shared-workers-terraform/configure-tentacle.sh
@@ -84,6 +84,13 @@ sudo ./aws/install
 # Install Java
 sudo apt install openjdk-17-jdk -y
 
+# Install Helm
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm -y
+
 # Pull worker tools image
 sudo docker pull #{Project.Docker.WorkerToolImage}:#{Project.Docker.WorkerToolImageTag}
 sudo docker pull ghcr.io/octopusdeploylabs/azure-workertools
