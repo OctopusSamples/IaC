43ea6e6b552fb4fe4ba7843365091f4e040bee6c
Twerthi <twerthi@hotmail.com>
2025-08-25 15:19:05 -0700

    Fixing terraform variable

--- octopus-samples-instances/gcp-base-infrastructure-terraform/service-account.tf
+++ octopus-samples-instances/gcp-base-infrastructure-terraform/service-account.tf
@@ -10,7 +10,7 @@ resource "time_sleep" "wait_30_seconds" {
 }
 
 resource "google_project_iam_member" "service_account_role" {
-    project = var.gcp_project
+    project = var.octopus_gcp_project
     role = "roles/cloudsql.instanceUser"
     member = "serviceAccount:${google_service_account.database_service_account.email}"
 }
