85ea053bf39a95b9800138c5b5e2e46232c88d24
Twerthi <twerthi@hotmail.com>
2025-08-27 09:53:16 -0700

    Fixing azurerm reference

--- octopus-samples-instances/shared-workers-terraform/azure/main.tf
+++ octopus-samples-instances/shared-workers-terraform/azure/main.tf
@@ -6,7 +6,8 @@ terraform {
     azurerm = {
       source = "hashicorp/azurerm"
 #      version = "~>2.0"
-      version = ">=3.0"
+#      version = ">=3.0"
+      version = ">=2.99"
     }
   }
   
