0693fc6e8e6f1ab3f8c5a96b52accbb5481f29e9
Twerthi <twerthi@hotmail.com>
2025-09-15 10:05:11 -0700

    Added attribute to wrong terraform resource

--- octopus-samples-instances/gcp-base-infrastructure-terraform/service-account.tf
+++ octopus-samples-instances/gcp-base-infrastructure-terraform/service-account.tf
@@ -1,6 +1,7 @@
 resource "google_service_account" "database_service_account" {
   account_id   = var.database_service_account_name
   display_name = "Database Service Account"
+  create_ignore_already_exists = true
 }
 
 resource "time_sleep" "wait_30_seconds" {
@@ -13,5 +14,4 @@ resource "google_project_iam_member" "service_account_role" {
     project = var.octopus_gcp_project
     role = "roles/cloudsql.instanceUser"
     member = "serviceAccount:${google_service_account.database_service_account.email}"
-    create_ignore_already_exists = true
 }
