6e9a59912ea8df182cdef62608e1ed76cbf5b536
Twerthi <twerthi@hotmail.com>
2025-09-15 09:53:36 -0700

    Adding service account ignore if already exists

--- octopus-samples-instances/gcp-base-infrastructure-terraform/service-account.tf
+++ octopus-samples-instances/gcp-base-infrastructure-terraform/service-account.tf
@@ -13,4 +13,5 @@ resource "google_project_iam_member" "service_account_role" {
     project = var.octopus_gcp_project
     role = "roles/cloudsql.instanceUser"
     member = "serviceAccount:${google_service_account.database_service_account.email}"
+    create_ignore_already_exists = true
 }
